04d3ee45281fd1c230afedc19c1b3baf44442ae4
Julian Lawrance <julian.lawrance@gmail.com>
2022-02-05 00:12:52 +0000

    Create client for BoreScore API

---
+++ src/PlayersList.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react"
+import { List } from "semantic-ui-react"
+
+interface Player {
+    id: number
+    name: string
+}
+
+interface PlayersListProps {
+
+}
+
+export const PlayersList = (props: PlayersListProps) => {
+    const [players, setPlayers] = useState<Player[]>([])
+
+    useEffect(() => {
+        fetch("http://localhost:8000/players")
+            .then(res => res.json())
+            .then((players: Player[]) => setPlayers(players))
+    }, [])
+
+    return (
+        <div>
+            <h2>Players</h2>
+
+            <List>
+                {players.map(p => (
+                    <List.Item>
+                        {p.name}
+                    </List.Item>
+                ))}
+            </List>
+        </div>
+    )
+}
