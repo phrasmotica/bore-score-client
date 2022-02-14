182f892f89bdec95bc8580b43440f808013173bc
Julian Lawrance <julian.lawrance@gmail.com>
2022-02-14 14:32:20 +0000

    Add component for adding a new player

--- src/PlayersList.tsx
+++ src/PlayersList.tsx
@@ -7,11 +7,18 @@ interface PlayersListProps {
     players: Player[]
     selectedPlayer: string | undefined
     setSelectedPlayer: (username: string | undefined) => void
+    setAddPlayer: () => void
 }
 
 export const PlayersList = (props: PlayersListProps) => (
     <div className="players-list">
         <List divided relaxed selection>
+            <List.Item onClick={props.setAddPlayer}>
+                <List.Content>
+                    <List.Content>Add Player</List.Content>
+                </List.Content>
+            </List.Item>
+
             {props.players.map(p => (
                 <PlayerCard
                     key={p.username}
--- src/PlayersPage.tsx
+++ src/PlayersPage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 
+import { AddPlayer } from "./AddPlayer"
 import { fetchPlayers } from "./FetchHelpers"
 import { Player } from "./Player"
 import { PlayerDetails } from "./PlayerDetails"
@@ -8,22 +9,34 @@ import { PlayersList } from "./PlayersList"
 export const PlayersPage = () => {
     const [players, setPlayers] = useState<Player[]>([])
     const [selectedPlayer, setSelectedPlayer] = useState<string>()
+    const [showAddPlayer, setShowAddPlayer] = useState(false)
 
     useEffect(() => {
         fetchPlayers()
             .then(setPlayers)
-    }, [])
+    }, [showAddPlayer])
 
     const getSelectedPlayer = () => players.find(p => p.username === selectedPlayer)
+
+    const setSelectedPlayerHandler = (username: string | undefined) => {
+        setSelectedPlayer(username)
+        setShowAddPlayer(false)
+    }
+
+    let details = <PlayerDetails player={getSelectedPlayer()} />
+    if (showAddPlayer) {
+        details = <AddPlayer addedPlayer={() => setShowAddPlayer(false)} />
+    }
 
     return (
         <div className="players-page">
             <PlayersList
                 players={players}
                 selectedPlayer={selectedPlayer}
-                setSelectedPlayer={setSelectedPlayer} />
+                setSelectedPlayer={setSelectedPlayerHandler}
+                setAddPlayer={() => setShowAddPlayer(true)} />
 
-            <PlayerDetails player={getSelectedPlayer()} />
+            {details}
         </div>
     )
 }
