ad378d873efef936885e08044c8c4157db9b5790
Julian Lawrance <julian.lawrance@gmail.com>
2022-02-15 17:18:11 +0000

    Add player search box to SelectablePlayersList

--- src/PlayersPage/SelectablePlayersList.tsx
+++ src/PlayersPage/SelectablePlayersList.tsx
@@ -1,4 +1,5 @@
-import { Icon, Menu } from "semantic-ui-react"
+import { useState } from "react"
+import { Input, Menu } from "semantic-ui-react"
 
 import { Player } from "../models/Player"
 
@@ -8,23 +9,47 @@ interface SelectablePlayersListProps {
     setSelectedPlayer: (username: string | undefined) => void
 }
 
-export const SelectablePlayersList = (props: SelectablePlayersListProps) => (
-    <div className="players-list">
-        <Menu vertical color="teal">
-                <Menu.Item header>
-                    <Icon name="user" />
-                    Select a player
+export const SelectablePlayersList = (props: SelectablePlayersListProps) => {
+    const [searchTerm, setSearchTerm] = useState("")
+
+    const matchesSearchTerm = (p: Player) => p.username.includes(searchTerm) || p.displayName.includes(searchTerm)
+
+    let playersToShow = props.players
+    if (searchTerm.length > 0) {
+        playersToShow = playersToShow.filter(matchesSearchTerm)
+    }
+
+    let listItems = playersToShow.map(p => (
+        <Menu.Item
+            key={p.username}
+            active={props.selectedPlayer === p.username}
+            onClick={() => props.setSelectedPlayer(p.username)}>
+            <span className="username">({p.username})</span>
+            <span className="display-name">{p.displayName}&nbsp;</span>
+        </Menu.Item>
+    ))
+
+    if (searchTerm.length > 0 && playersToShow.length < 1) {
+        listItems = [(
+            <Menu.Item>
+                No matching players!
+            </Menu.Item>
+        )]
+    }
+
+    return (
+        <div className="players-list">
+            <Menu vertical color="teal">
+                <Menu.Item>
+                    <Input
+                        icon="search"
+                        placeholder="Search players..."
+                        value={searchTerm}
+                        onChange={(e, { value }) => setSearchTerm(value)} />
                 </Menu.Item>
 
-                {props.players.map(p => (
-                    <Menu.Item
-                        key={p.username}
-                        active={props.selectedPlayer === p.username}
-                        onClick={() => props.setSelectedPlayer(p.username)}>
-                        <span className="username">({p.username})</span>
-                        <span className="display-name">{p.displayName}&nbsp;</span>
-                    </Menu.Item>
-                ))}
-        </Menu>
-    </div>
-)
+                {listItems}
+            </Menu>
+        </div>
+    )
+}
