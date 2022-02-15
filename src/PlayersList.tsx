bc9069e9dc6736231a3cff60ada57f63b6dcfce9
Julian Lawrance <julian.lawrance@gmail.com>
2022-02-15 17:15:21 +0000

    Add input for searching players to PlayersList

--- src/PlayersList.tsx
+++ src/PlayersList.tsx
@@ -1,4 +1,5 @@
-import { Icon, Menu } from "semantic-ui-react"
+import { useState } from "react"
+import { Input, Menu } from "semantic-ui-react"
 
 import { Player } from "./models/Player"
 
@@ -6,20 +7,44 @@ interface PlayersListProps {
     players: Player[]
 }
 
-export const PlayersList = (props: PlayersListProps) => (
-    <div className="players-list">
-        <Menu vertical>
-            <Menu.Item header>
-                <Icon name="user" />
-                Existing players
-            </Menu.Item>
+export const PlayersList = (props: PlayersListProps) => {
+    const [searchTerm, setSearchTerm] = useState("")
 
-            {props.players.map(p => (
-                <Menu.Item key={p.username}>
-                    <span className="username">({p.username})</span>
-                    <span className="display-name">{p.displayName}&nbsp;</span>
+    const matchesSearchTerm = (p: Player) => p.username.includes(searchTerm) || p.displayName.includes(searchTerm)
+
+    let playersToShow = props.players
+    if (searchTerm.length > 0) {
+        playersToShow = playersToShow.filter(matchesSearchTerm)
+    }
+
+    let listItems = playersToShow.map(p => (
+        <Menu.Item key={p.username}>
+            <span className="username">({p.username})</span>
+            <span className="display-name">{p.displayName}&nbsp;</span>
+        </Menu.Item>
+    ))
+
+    if (searchTerm.length > 0 && playersToShow.length < 1) {
+        listItems = [(
+            <Menu.Item>
+                No matching players!
+            </Menu.Item>
+        )]
+    }
+
+    return (
+        <div className="players-list">
+            <Menu vertical>
+                <Menu.Item>
+                    <Input
+                        icon="search"
+                        placeholder="Search players..."
+                        value={searchTerm}
+                        onChange={(e, { value }) => setSearchTerm(value)} />
                 </Menu.Item>
-            ))}
-        </Menu>
-    </div>
-)
+
+                {listItems}
+            </Menu>
+        </div>
+    )
+}
