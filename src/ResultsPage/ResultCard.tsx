f3955dd5b1faf2d1690e5e734fbde387a6c4aa73
Julian Lawrance <julian.lawrance@gmail.com>
2022-02-26 19:46:46 +0000

    Add hyperlinks to ResultCard headers

--- src/ResultsPage/ResultCard.tsx
+++ src/ResultsPage/ResultCard.tsx
@@ -31,43 +31,38 @@ export const ResultCard = (props: ResultCardProps) => {
     })
 
     const renderContent = () => {
-        switch (game?.winMethod) {
-            case WinMethodName.IndividualScore:
-                let scoresStr = playersWithScores.map(s => `${s.player}: ${s.score}`).join(", ")
-
-                return (
-                    <List.Content>
-                        <List.Header>{game?.displayName ?? r.gameName}</List.Header>
-                        <List.Description>{displayDateTimeValue(moment.unix(r.timestamp))}</List.Description>
-                        <List.Description>{scoresStr}</List.Description>
-                    </List.Content>
-                )
-
-            case WinMethodName.IndividualWinner:
-                let winnerStr = playersWithScores.map(s => `${s.player} ${s.isWinner ? "won" : "lost"}`).join(", ")
-
-                return (
-                    <List.Content>
-                        <List.Header>{game?.displayName ?? r.gameName}</List.Header>
-                        <List.Description>{displayDateTimeValue(moment.unix(r.timestamp))}</List.Description>
-                        <List.Description>{winnerStr}</List.Description>
-                    </List.Content>
-                )
-
-            case WinMethodName.CooperativeScore:
-                let playersStr = playersWithScores.slice(0, -1).map(s => s.player).join(", ")
-                playersStr += ` & ${playersWithScores.at(-1)?.player}`
-
-                return (
-                    <List.Content>
-                        <List.Header>{game?.displayName ?? r.gameName}</List.Header>
-                        <List.Description>{displayDateTimeValue(moment.unix(r.timestamp))}</List.Description>
-                        <List.Description>{playersStr}: {r.cooperativeScore}</List.Description>
-                    </List.Content>
-                )
+        if (game === undefined) {
+            return null
         }
 
-        return null
+        let scoreStr = ""
+
+        switch (game?.winMethod) {
+            case WinMethodName.IndividualScore:
+                scoreStr = playersWithScores.map(s => `${s.player}: ${s.score}`).join(", ")
+                break
+
+            case WinMethodName.IndividualWinner:
+                scoreStr = playersWithScores.map(s => `${s.player} ${s.isWinner ? "won" : "lost"}`).join(", ")
+                break
+
+            case WinMethodName.CooperativeScore:
+                scoreStr = playersWithScores.slice(0, -1).map(s => s.player).join(", ")
+                scoreStr += ` & ${playersWithScores.at(-1)?.player}: ${r.cooperativeScore}`
+                break
+        }
+
+        return (
+            <List.Content>
+                <List.Header>
+                    <a href={`/games?game=${game.name}`}>
+                        {game.displayName ?? r.gameName}
+                    </a>
+                </List.Header>
+                <List.Description>{displayDateTimeValue(moment.unix(r.timestamp))}</List.Description>
+                <List.Description>{scoreStr}</List.Description>
+            </List.Content>
+        )
     }
 
     return (
