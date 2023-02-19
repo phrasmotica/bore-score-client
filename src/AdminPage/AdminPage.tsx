c353f214f0ce1505714c71898903d9304257a035
Julian L <julian.lawrance@gmail.com>
2023-02-19 18:59:09 +0000

    Move button for adding a new game to the admin page

--- src/AdminPage/AdminPage.tsx
+++ src/AdminPage/AdminPage.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { useNavigate } from "react-router"
 import { Button, Icon } from "semantic-ui-react"
 
 import { AddPlayerModal } from "../AddPlayerModal/AddPlayerModal"
@@ -7,6 +8,8 @@ import { useTitle } from "../Hooks"
 
 export const AdminPage = () => {
     useTitle("Admin")
+
+    const navigate = useNavigate()
 
     const [showAddPlayerModal, setShowAddPlayerModal] = useState(false)
 
@@ -21,6 +24,14 @@ export const AdminPage = () => {
                 color="yellow"
                 onClick={() => setShowAddPlayerModal(true)}>
                 <span>Add New Player&nbsp;</span>
+                <Icon name="plus" />
+            </Button>
+
+            <Button
+                icon
+                color="yellow"
+                onClick={() => navigate("/add-game")}>
+                <span>Add New Game&nbsp;</span>
                 <Icon name="plus" />
             </Button>
         </div>
