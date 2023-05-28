d0bfa1b444b0f90b1447cb924f80efb56e0578d4
Julian L <julian.lawrance@gmail.com>
2023-05-28 19:13:27 +0100

    Create dropdown for profile/account-related things when logged in

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,16 +1,17 @@
 import { useEffect } from "react"
 import { Link, useLocation } from "react-router-dom"
-import { Menu, Button } from "semantic-ui-react"
+import { Menu, Dropdown } from "semantic-ui-react"
 
 import { parseToken, removeToken } from "./Auth"
 
 export const Navbar = () => {
     const token = parseToken()
-    const username = token?.username || ""
 
     const logOut = () => {
         removeToken()
         window.dispatchEvent(new Event("storage"))
+
+        // TODO: redirect to a non-authed page. Maybe login page or home page?
     }
 
     useEffect(() => {
@@ -70,17 +71,25 @@ export const Navbar = () => {
             </Menu.Menu>}
 
             {token && <Menu.Menu position="right">
-                <Menu.Item>
-                    <Link to="/me">
-                        <strong>{username}</strong>
-                    </Link>
-                </Menu.Item>
+                <Dropdown item simple text="My Account">
+                    <Dropdown.Menu>
+                        <Dropdown.Item>
+                            <Link to="/me">
+                                My Profile
+                            </Link>
+                        </Dropdown.Item>
 
-                <Menu.Item>
-                    <Button color="red" onClick={logOut}>
-                        Log Out
-                    </Button>
-                </Menu.Item>
+                        <Dropdown.Item>
+                            <Link to="/me-edit">
+                                Account Settings
+                            </Link>
+                        </Dropdown.Item>
+
+                        <Dropdown.Item onClick={logOut}>
+                            Log Out
+                        </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
             </Menu.Menu>}
         </Menu>
     )
