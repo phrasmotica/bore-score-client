fa137f460eca4ba01d8fa9b3b357db59a610d990
Julian Lawrance <julian.lawrance@gmail.com>
2023-05-30 11:14:00 +0100

    Add played count to leaderboard-related models

--- src/models/Leaderboard.ts
+++ src/models/Leaderboard.ts
@@ -1,10 +1,12 @@
 export interface Leaderboard {
     groupId: string
     gameId: string
+    playedCount: number
     leaderboard: Rank[]
 }
 
 export interface Rank {
     username: string
     pointsScored: number
+    playedCount: number
 }
